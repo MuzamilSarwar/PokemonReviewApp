e42ae7cabe1600f6bf7bddd813c720bc62db84fc
Muzamil <muzamilsarwar415@gmail.com>
2023-06-10 14:50:48 +0500

    Add project files.

---
+++ PokemonReviewApp/Models/Pokimon.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+namespace PokemonReviewApp.Models
+{
+    public class Pokimon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public int pokimonId { get; set; }
+        public Pokimon? pokimon { get; set; }
+
+        public int reviewerId { get; set; }
+        public Reviewer? reviewer { get; set; }
+    }
+}
